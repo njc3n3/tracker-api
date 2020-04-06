e8724962025ac3a3520528b501d9ffecf7679cdb
Nick Coffey <njc3n3@gmail.com>
2020-04-06 16:14:26 -0500

    added mlab string todo

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import {ApolloServer} from 'apollo-server';
 import {typeDefs, resolvers} from './schema';
 
 mongoose.connect(
-  'mongodb://tracker:tracker123@ds147942.mlab.com:47942/lift-tracker-gql',
+  'mongodb://tracker:tracker123@ds147942.mlab.com:47942/lift-tracker-gql', // TODO replace with environment variables and new username and password
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
