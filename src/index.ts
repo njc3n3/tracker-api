d2d441d8888841d47273a7a72a1b487332488f05
Nick Coffey <ncoffey@coolfiresolutions.com>
2020-10-15 13:22:01 -0500

    change mongo string to Atlas

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,14 @@ import mongoose from 'mongoose'
 import {ApolloServer} from 'apollo-server'
 import {typeDefs, resolvers} from './schema'
 
-mongoose.connect(`mongodb://tracker:${process.env.TRACKER_DB_PW}@ds147942.mlab.com:47942/lift-tracker-gql`, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false
-})
+mongoose.connect(
+  `mongodb+srv://nicholasjcoffey:${process.env.TRACKER_DB_PW}@cluster0.j0ien.mongodb.net/tracker?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  }
+)
 mongoose.connection.once('open', () => {
   // tslint:disable-next-line: no-console
   console.log('Connected to database')
